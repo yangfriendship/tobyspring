8e6d6b0abbd4508b1a02cde398081715d5041426
yangfriendship <yangfriendship@naver.com>
2021-01-09 13:00:51 +0900

    feat : 1.1

---
+++ src/main/java/springbook/user/UserDao.java
@@ -0,0 +1,56 @@
+package springbook.user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection(
+            "jdbc:h2:tcp://localhost/~/test", "sa", "");
+        PreparedStatement ps = c.prepareStatement(
+            "insert into users(id, name, password) values (?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+/*
+        Class.forName("org.h2.Driver");
+    Connection c = DriverManager.getConnection(
+        "jdbc:h2:tcp://localhost/~/test");
+*/
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection(
+            "jdbc:h2:tcp://localhost/~/test", "sa", "");
+
+        PreparedStatement ps = c
+            .prepareStatement("select * from users where id =?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+        return user;
+    }
+
+}
---
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -0,0 +1,31 @@
+package springbook.user;
+
+import java.sql.SQLException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserDaoTest {
+
+    private UserDao userDao;
+
+    @Before
+    public void setUp() {
+        this.userDao = new UserDao();
+    }
+
+    @Test
+    public void addTest() throws SQLException, ClassNotFoundException {
+
+        User user = new User("1", "name", "ps");
+        userDao.add(user);
+
+        User find = userDao.get(user.getId());
+
+        Assert.assertEquals(user.getId(),find.getId());
+        Assert.assertEquals(user.getName(),find.getName());
+        Assert.assertEquals(user.getPassword(),find.getPassword());
+
+    }
+
+}
