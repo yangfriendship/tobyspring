ae03c3ea6394472c9908aaa45d3d30d730bec292
yangfriendship <yangfriendship@naver.com>
2021-01-15 14:39:46 +0900

    feat : 6.7.x

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,16 +1,20 @@
 package springbook.user.service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 import springbook.user.User;
 
+@Transactional
 public interface UserService {
 
     void add(User user);
 
     void add(List<User> user);
 
+    @Transactional(readOnly = true)
     User get(String id);
 
+    @Transactional(readOnly = true)
     List<User> getAll();
 
     void deleteAll();
